0c0c381a78afcfffcf0f1db852507ef95a6ecadb
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-02-04 22:18:06 +0300

    QuickStart.WebApi

---
+++ QuickStart/QuickStart.WebApi/Context/QuickStartContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickStart.WebApi.Entity;
+
+namespace QuickStart.WebApi.Context
+{
+    public class QuickStartContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Ilyas; initial Catalog=DbQuickStartAkademiq; integrated security=true; TrustServerCertificate=True");
+        }
+
+        public DbSet<Service> Services { get; set; }
+
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
---
+++ QuickStart/QuickStart.WebApi/Entity/Testimonial.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickStart.WebApi.Entity
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string FulllName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Rate { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
