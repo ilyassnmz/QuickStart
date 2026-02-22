10e3cb7674fde52e201695d09af7c129a23de7a0
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-02-22 17:43:10 +0300

    Add Post & Delete

--- QuickStart/QuickStart.WebApi/Controller/ServiceController.cs
+++ QuickStart/QuickStart.WebApi/Controller/ServiceController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using QuickStart.WebApi.Context;
+using QuickStart.WebApi.Entity;
 
-namespace QuickStart.WebApi.Controller
+namespace QuickStart.WepApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -20,6 +21,48 @@ namespace QuickStart.WebApi.Controller
         {
             var value = _context.Services.ToList();
             return Ok(value);
+        }
+
+        [HttpGet("ServiceCount")]
+
+        public IActionResult ServiceCount()
+        {
+            var value = _context.Services.Count();
+            return Ok(value);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var Service = _context.Services.Find(id);
+            return Ok(Service);
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateService(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges();
+            return Ok("ekleme işlemi başarı ile gerçekleşti");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _context.Services.Update(service);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarı ile gerçekleşti");
+        }
+
+        [HttpDelete]
+
+        public IActionResult DeleteService(int id)
+        {
+            var value = _context.Services.Find(id);
+            _context.Services.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarı ile gerçekleşti");
+
         }
     }
 }
