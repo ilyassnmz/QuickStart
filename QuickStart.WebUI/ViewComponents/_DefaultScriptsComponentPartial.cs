09c8197382411bb78cad7a1e9a0cb87a049e081d
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-03-02 21:57:46 +0300

    Componentlere ayrıldı

---
+++ QuickStart.WebUI/ViewComponents/_DefaultScriptsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickStart.WebUI.ViewComponents
+{
+    public class _DefaultScriptsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
