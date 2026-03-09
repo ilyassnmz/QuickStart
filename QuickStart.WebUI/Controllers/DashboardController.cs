c332386082fa295c5850d2e54deae7f97d04e5b8
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-03-09 22:39:02 +0300

    Dashboard Controller eklendi

---
+++ QuickStart.WebUI/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickStart.WebUI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DashboardController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7121/api/Testimonial/TestimonialCount");
+            var JsonData = await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.TestimonialCount = JsonData;
+
+            var responseMessage1 = await client.GetAsync("https://localhost:7121/api/Service/ServiceCount");
+            var JsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+            ViewBag.ServiceCount = JsonData1;
+            return View();
+        }
+    }
+}
--- QuickStart.WebUI/ViewComponents/_AdminLayoutNavbarComponentPartial.cs
+++ QuickStart.WebUI/ViewComponents/_AdminLayoutNavbarComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QuickStart.WebUI.Dtos.Notifications;
 
 namespace QuickStart.WebUI.ViewComponents
 {
     public class _AdminLayoutNavbarComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminLayoutNavbarComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7121/api/Notification/GetNotificationListwithNotificationType");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultNotificationWithNotificationTypeDto>>(jsonData);
+                return View(values);
+
+            }
             return View();
         }
     }
