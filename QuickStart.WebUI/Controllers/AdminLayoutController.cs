89c22e9d0a1fe91809d879e4df2edad1cee9048a
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-03-14 16:33:59 +0300

    Admin Dashboard Dinamik hale getirildi

--- QuickStart.WebUI/Controllers/AdminLayoutController.cs
+++ QuickStart.WebUI/Controllers/AdminLayoutController.cs
@@ -1,11 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace QuickStart.WebUI.Controllers
 {
     public class AdminLayoutController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminLayoutController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var baseUrl = "https://localhost:7121/api/";
+
+            var endpoints = new[]
+            {
+                "About", "ContactInfo", "FAQ", "Feature", "Message",
+                "Notification", "NotificationType", "Service", "Slider", "Testimonial"
+            };
+
+            var counts = new Dictionary<string, int>();
+
+            foreach (var endpoint in endpoints)
+            {
+                try
+                {
+                    var responseMessage = await client.GetAsync(baseUrl + endpoint);
+                    if (responseMessage.IsSuccessStatusCode)
+                    {
+                        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                        var array = JsonSerializer.Deserialize<JsonArray>(jsonData);
+                        counts[endpoint] = array?.Count ?? 0;
+                    }
+                    else
+                    {
+                        counts[endpoint] = 0;
+                    }
+                }
+                catch
+                {
+                    counts[endpoint] = 0;
+                }
+            }
+
+            ViewBag.Counts = counts;
             return View();
         }
     }
